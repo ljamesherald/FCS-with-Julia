04796c53cb2ad5ec5621f5648e455a6dca29becd
ljamesherald <ljamesherald@gmail.com>
2018-11-27 11:57:24 -0500

    Fixed the test file

--- Flow Cytometry Data Analyzer/FCSFiles/test/runtests.jl
+++ Flow Cytometry Data Analyzer/FCSFiles/test/runtests.jl
@@ -2,6 +2,6 @@ using FCSFiles
 using FileIO
 using Test
 
-flowrun = load("testdata/BD-FACS-Aria-II.fcs")
+flowrun = load("/home/ljherald/github/FCS-with-Julia/Flow Cytometry Data Analyzer/FCSFiles/test/testdata/BD-FACS-Aria-II.fcs")
 
 @test length(flowrun["SSC-A"]) == 100000
