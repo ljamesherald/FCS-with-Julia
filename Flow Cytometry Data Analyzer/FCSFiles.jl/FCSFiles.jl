c019a2b4e4ce8fc6fb9d31823439d3e84e38e727
ljamesherald <ljamesherald@gmail.com>
2018-11-27 08:38:12 -0500

    Using code from a library found

---
+++ Flow Cytometry Data Analyzer/FCSFiles.jl/FCSFiles.jl
@@ -0,0 +1,26 @@
+module FCSFiles
+
+using FileIO
+
+include("type.jl")
+include("utils.jl")
+include("parse.jl")
+
+export FlowSample
+
+try
+    add_format(format"FCS", "FCS", [".fcs"])
+end
+
+function FileIO.load(f::File{format"FCS"})
+    open(f) do io
+        offsets = parse_header(io)
+
+        text_mappings = parse_text(io, offsets[1], offsets[2])
+        verify_text(text_mappings)
+
+        return parse_data(io, offsets[3], offsets[4], text_mappings)
+    end
+end
+
+end # module
