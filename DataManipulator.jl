00e4aa9657d5636541aa56ba57d8cf17b4a38749
ljamesherald <ljamesherald@gmail.com>
2018-11-27 23:57:40 -0500

    I fixed it

--- DataManipulator.jl
+++ DataManipulator.jl
@@ -1,4 +1,13 @@
 using FileIO
 using FCSFiles
+using Gadfly
 
-flowrun = load("/home/ljherald/Documents/FCS files/FCS_files_to_match_prior_Excel_worksheet_-_061812/Trog-ActinInhib-061812.021")
+flowrun = load(raw"C:\Users\ljame\Documents\GitHub\FCS-with-Julia\FCS_data\BD-FACS-Aria-II.fcs")
+
+
+
+p = plot(x=flowrun["FSC-A"], y=flowrun["SSC-A"], Geom.histogram2d,
+Guide.xlabel("FSC-A"), Guide.ylabel("SSC-A"), Coord.cartesian(xmin=0, ymin=0))
+
+#import Cairo, Fontconfig
+#draw(PNG("BD-FACS-Aria-II-Graph.png", 10cm, 7cm, dpi=300), p)
